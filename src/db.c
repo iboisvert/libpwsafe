2b38eaa52963392b144d04dfe0409e31b076146f
Ian Boisvert <boisvert.ian@gmail.com>
2023-01-06 07:36:57 +0000

    Decrypt database

--- src/db.c
+++ src/db.c
@@ -1,12 +1,14 @@
 /* Copyright 2023 Ian Boisvert */
 #include "pwsafe.h"
 #include <assert.h>
-#include <arpa/inet.h> // for byte order
-#include <nettle/sha1.h>
 #include <nettle/blowfish.h>
+#include <nettle/sha1.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// IMB 2023-01-01 Most of the code here is
+// adapted from code by Nicolas Dade https://github.com/nsd20463/pwsafe
 
 // Prevent memset from being optimized out
 typedef void *(*fmemset)(void *, int, size_t);
@@ -18,28 +20,53 @@ static volatile fmemset memset_func = memset;
 // typedef uint32_t (*fboswap)(uint32_t);
 // static fboswap host_to_le = identity;
 
-// A magic number used to verify the header
+// A magic number used to verify the handle in API calls
 static const unsigned char DB_MAGIC[16] = {
     0x05, 0xf5, 0x4d, 0x91, 0xe8, 0xb6, 0x44, 0x4b, 0xb4, 0xed, 0xba, 0xd4, 0x2a, 0x5e, 0x03, 0x49};
 
+const char * const PWSAFE_V2_NAME_MAGIC = 
+    " !!!Version 2 File Format!!! Please upgrade to PasswordSafe 2.0 or later";
+
+typedef uint8_t Block[8];
+static const size_t BLOCK_SIZE = sizeof(Block);
+
 typedef struct
 {
-    unsigned char random[8];
+    Block random;
     unsigned char hash[SHA1_DIGEST_SIZE]; // 20
     unsigned char salt[SHA1_DIGEST_SIZE]; // 20
-    unsigned char iv[8];
+    Block iv;
 } Header;
 
 typedef struct
 {
     unsigned char magic[16];
     unsigned char ver; // Struct version
-    FILE *dbfile;
-    Header header;
-    unsigned char key[SHA1_DIGEST_SIZE];
+    FILE *db_file;
+    Header db_header;
+    PWS_DB_VERSION db_vers;
+    struct blowfish_ctx bf_ctx;
+    Block cbc;
 } PwsDb;
 
-typedef uint32_t Block[2];
+typedef struct
+{
+    uint8_t type;
+    char *value;
+} PwsDbField;
+
+typedef struct
+{
+    PwsDbField *fields;
+} PwsDbRecord;
+
+/** Swap block byte order le--be */
+static void swap_byte_order(Block b)
+{
+    uint32_t *l = (uint32_t *)(b), *h = (uint32_t *)(b + 4);
+    *l = __builtin_bswap32(*l);
+    *h = __builtin_bswap32(*h);
+}
 
 static PwsDb *db_alloc()
 {
@@ -47,27 +74,24 @@ static PwsDb *db_alloc()
     pdb = malloc(sizeof(PwsDb));
     memcpy(pdb->magic, DB_MAGIC, sizeof(DB_MAGIC));
     pdb->ver = 1;
-    pdb->dbfile = NULL;
+    pdb->db_file = NULL;
     return pdb;
 }
 
 static void db_free(PwsDb *pdb)
 {
-    memset(pdb->magic, 0, sizeof(DB_MAGIC));
-    if (pdb->dbfile)
+    if (pdb->db_file)
     {
-        fclose(pdb->dbfile);
-        pdb->dbfile = NULL;
+        fclose(pdb->db_file);
     }
-    memset_func(pdb->key, 0, sizeof(pdb->key));
+    memset_func(pdb, 0, sizeof(*pdb));
     free(pdb);
 }
 
 static _Bool db_read_header(PwsDb *pdb)
 {
-    // IMB 2023-01-01 Adapted from code by Nicolas Dade https://github.com/nsd20463/pwsafe
-    FILE *f = pdb->dbfile;
-    Header *hdr = &pdb->header;
+    FILE *f = pdb->db_file;
+    Header *hdr = &pdb->db_header;
     if (fread(hdr->random, 1, sizeof(hdr->random), f) != sizeof(hdr->random) ||
         fread(hdr->hash, 1, sizeof(hdr->hash), f) != sizeof(hdr->hash) ||
         fread(hdr->salt, 1, sizeof(hdr->salt), f) != sizeof(hdr->salt) ||
@@ -78,27 +102,140 @@ static _Bool db_read_header(PwsDb *pdb)
     return true;
 }
 
+static _Bool db_read_block(PwsDb *pdb, Block block, PWS_RESULT_CODE *rc)
+{
+    *rc = PWS_SUCCESS;
+
+    FILE *f = pdb->db_file;
+    size_t len = fread(block, 1, BLOCK_SIZE, f);
+    if (len != BLOCK_SIZE)
+    {
+        if (feof(f))
+        {
+            *rc = PWS_ERR_CORRUPT_DB;
+        }
+        else
+        {
+            *rc = PWS_ERR_READ;
+        }
+        return false;
+    }
+
+    Block save_block;
+    memcpy(save_block, block, BLOCK_SIZE);
+
+    // There is something very fishy about having to 
+    // swap byte-order in order to match OpenSSL
+    swap_byte_order(block);
+    blowfish_decrypt(&pdb->bf_ctx, BLOCK_SIZE, (uint8_t *)block, (const uint8_t *)block);
+    swap_byte_order(block);
+
+    const uint8_t *cbc = pdb->cbc;
+    for (size_t i = 0; i < BLOCK_SIZE; ++i)
+    {
+        block[i] = block[i] ^ cbc[i];
+    }
+    memcpy(pdb->cbc, save_block, BLOCK_SIZE);
+
+    memset_func(save_block, 0, BLOCK_SIZE);
+    
+    return true;
+}
+
+static _Bool db_read_next_field(PwsDb *pdb, uint8_t *type, char **str, PWS_RESULT_CODE *rc)
+{
+    *type = FT_END;
+    *str = NULL;
+
+    Block block;
+    if (!db_read_block(pdb, block, rc))
+        return false;
+
+    *type = block[4];
+    int32_t len = *(int32_t *)(block);
+    if (len < 0 || len > 64 * 1024)
+    {
+        *rc = PWS_ERR_CORRUPT_DB;
+        return false;
+    }
+
+    char *s = *str = malloc(len + 1);
+    size_t nblocks = (len + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    for (size_t i = 0; i < nblocks; ++i)
+    {
+        if (!db_read_block(pdb, block, rc))
+        {
+            free(*str);
+            *str = NULL;
+            return false;
+        }
+        memcpy(s + i * BLOCK_SIZE, block, BLOCK_SIZE);
+    }
+    s[len] = 0;
+
+    return true;
+}
+
+static _Bool db_read_next_v1_record(PwsDb *pdb, PWS_RESULT_CODE *rc)
+{
+    uint8_t unused;
+    char *value;
+
+    _Bool status = db_read_next_field(pdb, &unused, &value, rc);
+    status = status && db_read_next_field(pdb, &unused, &value, rc);
+    status = status && db_read_next_field(pdb, &unused, &value, rc);
+
+    return status;
+}
+
+static _Bool db_read_next_v2_record(PwsDb *pdb, PWS_RESULT_CODE *rc)
+{
+    _Bool status = true;
+    FILE *f = pdb->db_file;
+
+    int nfields = 1000;
+    while (status && !feof(f) && nfields-- > 1)
+    {
+        uint8_t type;
+        char *value;
+        status = db_read_next_field(pdb, &type, &value, rc);
+        switch(type) {
+            case FT_END: goto done;
+        }
+    }
+    if (nfields == 0)
+    {
+        *rc = PWS_ERR_CORRUPT_DB;
+    }
+done:
+    return status;
+}
+
+static _Bool db_read_next_record(PwsDb *pdb, PWS_RESULT_CODE *rc)
+{
+    if (pdb->db_vers == PWSAFE_DB_V2)
+    {
+        return db_read_next_v2_record(pdb, rc);
+    }
+    else
+    {
+        return db_read_next_v1_record(pdb, rc);
+    }
+}
+
 static void db_compute_key(PwsDb *pdb, const char *pw)
 {
-    Header *h = &pdb->header;
-
-    // IMB 2023-01-01 Adapted from code by Nicolas Dade https://github.com/nsd20463/pwsafe
-    // cbc.read(h.iv,sizeof(h.iv));
-    // SHA_CTX sha;
-    // SHA1_Init(&sha);
-    // SHA1_Update(&sha, pw.data(), pw.length());
-    // SHA1_Update(&sha, h.salt, sizeof(h.salt));
-    // unsigned char key[SHA_DIGEST_LENGTH];
-    // SHA1_Final(key, &sha);
-    // BF_set_key(&bf, sizeof(key), key);
-    // memset(&sha,0,sizeof(sha));
-    // memset(&key,0,sizeof(key));
+    Header *h = &pdb->db_header;
+    memcpy(pdb->cbc, h->iv, sizeof(pdb->cbc));
     struct sha1_ctx ctx;
     sha1_init(&ctx);
     size_t pw_len = strlen(pw);
     sha1_update(&ctx, pw_len, (const uint8_t *)pw);
     sha1_update(&ctx, sizeof(h->salt), h->salt);
-    sha1_digest(&ctx, sizeof(pdb->key), pdb->key);
+    uint8_t key[SHA1_DIGEST_SIZE];
+    sha1_digest(&ctx, sizeof(key), key);
+    blowfish_set_key(&pdb->bf_ctx, sizeof(key), key);
 }
 
 /** Sanity checks */
@@ -150,7 +287,7 @@ PWSAFE_EXTERN PWSHANDLE pws_db_open(const char *pathname, const char *password, 
     }
 
     PwsDb *pdb = db_alloc();
-    pdb->dbfile = dbfile;
+    pdb->db_file = dbfile;
 
     if (!db_read_header(pdb))
     {
@@ -161,7 +298,8 @@ PWSAFE_EXTERN PWSHANDLE pws_db_open(const char *pathname, const char *password, 
     }
     db_compute_key(pdb, password);
 
-    if (rc) *rc = PWS_SUCCESS;
+    if (rc)
+        *rc = PWS_SUCCESS;
     return pdb;
 }
 
@@ -179,9 +317,8 @@ PWSAFE_EXTERN _Bool pws_db_check_password(PWSHANDLE hdb, const char *pw)
     if (!check_hdb(hdb, NULL))
         return false;
 
-    // IMB 2023-01-01 Adapted from code by Nicolas Dade https://github.com/nsd20463/pwsafe
     PwsDb *pdb = (PwsDb *)hdb;
-    Header *h = &pdb->header;
+    Header *h = &pdb->db_header;
 
     // generate test hash from random and passphrase
     // I am mystified as to why Bruce uses these extra 2 zero bytes in the hashes
@@ -199,20 +336,18 @@ PWSAFE_EXTERN _Bool pws_db_check_password(PWSHANDLE hdb, const char *pw)
     blowfish_set_key(&bf_ctx, sizeof(key), key);
 
     Block block;
-    memcpy(&block, h->random, sizeof(block));
+    memcpy(&block, h->random, BLOCK_SIZE);
 
     // For nettle to match OpenSSL we must swap byte order before and after encrypt
     // TODO: test this on a big-endian machine
-    block[0] = __builtin_bswap32(block[0]);
-    block[1] = __builtin_bswap32(block[1]);
+    swap_byte_order(block);
 
     // to mimic passwordsafe I use BF_encrypt() directly, but that means I have to pretend that I am on a little-endian
     // machine b/c passwordsafe assumes a i386
     for (int i = 0; i < 1000; ++i)
-        blowfish_encrypt(&bf_ctx, sizeof(block), (uint8_t *)&block, (const uint8_t *)&block);
+        blowfish_encrypt(&bf_ctx, BLOCK_SIZE, (uint8_t *)&block, (const uint8_t *)&block);
 
-    block[0] = __builtin_bswap32(block[0]);
-    block[1] = __builtin_bswap32(block[1]);
+    swap_byte_order(block);
 
     // Now comes a sad part: I have to hack to mimic the original passwordsafe which contains what I believe
     // is a bug. passwordsafe used its own blowfish and sha1 libraries, and its version of SHA1Final()
@@ -225,14 +360,30 @@ PWSAFE_EXTERN _Bool pws_db_check_password(PWSHANDLE hdb, const char *pw)
     // and watch what the variables are doing; sometimes it is eye opening!
     sha1_init(&sha_ctx);
     memset_func(sha_ctx.state, 0, sizeof(sha_ctx.state));
-    sha1_update(&sha_ctx, sizeof(block), (const uint8_t *)block);
+    sha1_update(&sha_ctx, BLOCK_SIZE, (const uint8_t *)block);
     sha1_update(&sha_ctx, sizeof(zeros), zeros);
     unsigned char test_hash[SHA1_DIGEST_SIZE];
     sha1_digest(&sha_ctx, sizeof(test_hash), test_hash);
 
     memset_func(key, 0, sizeof(key));
+    memset_func(&bf_ctx, 0, sizeof(bf_ctx));
 
     _Bool equal = memcmp(test_hash, h->hash, sizeof(h->hash)) == 0 ? true : false;
 
     return equal;
+}
+
+PWSAFE_EXTERN _Bool pws_db_read_accounts(PWSHANDLE hdb, PWS_RESULT_CODE *rc)
+{
+    if (!check_hdb(hdb, NULL))
+        return false;
+
+    PwsDb *pdb = (PwsDb *)hdb;
+
+    while (db_read_next_record(pdb, rc))
+    {
+//  PWSAFE_V2_NAME_MAGIC       
+    }
+
+    return false;
 }
--- test/test.cc
+++ test/test.cc
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "pwsafe.h"
 
-TEST(Test, OpenDbV1)
+TEST(Test, OpenEmptyDbV1)
 {
     PWS_RESULT_CODE rc;
     PWSHANDLE hdb = pws_db_open("data/test-v1-empty.dat", "password", &rc);
@@ -15,7 +15,7 @@ TEST(Test, OpenDbV1)
     pws_db_close(hdb, &rc);
 }
 
-TEST(Test, OpenDbV2)
+TEST(Test, OpenEmptyDbV2)
 {
     PWS_RESULT_CODE rc;
     PWSHANDLE hdb = pws_db_open("data/test-v2-empty.dat", "password", &rc);
@@ -23,6 +23,18 @@ TEST(Test, OpenDbV2)
     ASSERT_EQ(0, rc);
 
     ASSERT_TRUE(pws_db_check_password(hdb, "password"));
+
+    pws_db_close(hdb, &rc);
+}
+
+TEST(Test, ReadDbV2)
+{
+    PWS_RESULT_CODE rc;
+    PWSHANDLE hdb = pws_db_open("data/test-v2.dat", "password", &rc);
+    ASSERT_NE((void*)NULL, hdb);
+    ASSERT_EQ(0, rc);
+
+    ASSERT_TRUE(pws_db_read_accounts(hdb, &rc));
 
     pws_db_close(hdb, &rc);
 }
